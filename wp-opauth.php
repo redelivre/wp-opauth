f580c31c43ad239ed70964c584c9576f2991b566
Flávio Zavan <flaviozavan@gmail.com>
2013-12-10 13:48:39 -0200

    Add the plugin description

---
+++ wp-opauth.php
@@ -0,0 +1,10 @@
+<?php
+	/*
+	Plugin Name: Wordpress Opauth
+	Plugin URI: http://www.ethymos.com.br
+	Description: Adds Opauth (opauth.org) support
+	Author: Ethymos
+	Version: 0.01
+	Text Domain: wp-opauth
+	*/
+?>
