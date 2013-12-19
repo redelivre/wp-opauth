707bade4b72740f9c5ed4b7c66c1b542b5c91ec7
Flávio Zavan <flaviozavan@gmail.com>
2013-12-19 18:51:41 -0200

    Catch callback

--- wpopauth.php
+++ wpopauth.php
@@ -22,13 +22,46 @@ class WPOpauth
 
 	public function template_redirect()
 	{
+		/* Run the callback */
+		$callbackURI = str_replace('{path}',
+				$this->opauth->config['path'],
+				$this->opauth->config['callback_url']);
+		if ($_SERVER['REQUEST_URI'] === $callbackURI)
+		{
+			$this->callback();
+			exit;
+		}
 		/* Run opauth only when in the right path. */
-		if (preg_match(
+		else if (preg_match(
 					'/^' . preg_quote($this->opauth->config['path'], '/') . '/',
 					$_SERVER['REQUEST_URI']))
 		{
 			$this->opauth->run();
 		}
+	}
+
+	public function callback()
+	{
+		$response = null;
+
+		switch($this->opauth->env['callback_transport']) {
+			case 'session':
+				session_start();
+				if (array_key_exists('opauth', $_SESSION))
+				{
+					$response = $_SESSION['opauth'];
+					unset($_SESSION['opauth']);
+				}
+				break;
+			case 'post':
+				$response = unserialize(base64_decode($_POST['opauth']));
+				break;
+			case 'get':
+				$response = unserialize(base64_decode($_GET['opauth']));
+				break;
+		}
+
+		var_dump($response);
 	}
 }
 
