9a0a863c67ad8474fc898d02fcd2e1fe5ad32509
Flávio Zavan <flaviozavan@gmail.com>
2013-12-20 14:51:12 -0200

    Redirect requests to and from the network

--- wpopauth.php
+++ wpopauth.php
@@ -6,12 +6,26 @@ class WPOpauth
 
 	public function __construct($config = array())
 	{
+		/* Override Opauth's path, callback_url and callback_transport variables */
+		$config['path'] = preg_replace('/^' . preg_quote(site_url(), '/') . '/',
+				'', WP_PLUGIN_URL) . "/wp-opauth/auth/";
+		$config['callback_url'] = $config['path'] . 'callback.php';
+		$config['callback_transport'] = 'post';
+
+		/* Set the state for multisite support */
+		foreach ($config['Strategy'] as &$strategy)
+		{
+			$strategy['state'] = get_current_blog_id();
+		}
+
 		$this->opauth = new Opauth($config, false);
-		add_action('login_form', array($this, 'login_form'));
-		add_action('template_redirect', array($this, 'template_redirect'));
+		/* Override host to enable multisite support */
+		$this->opauth->env['host'] = preg_replace('/\/$/', '', network_site_url());
+		add_action('login_form', array($this, 'loginForm'));
+		add_action('init', array($this, 'init'));
 	}
 
-	public function login_form()
+	private function loginForm()
 	{
 		$strategies = $this->opauth->strategyMap;
 
@@ -20,48 +34,66 @@ class WPOpauth
 			. DIRECTORY_SEPARATOR . 'login_form.php';
 	}
 
-	public function template_redirect()
+	public function init()
 	{
 		/* Run the callback */
-		$callbackURI = str_replace('{path}',
-				$this->opauth->config['path'],
-				$this->opauth->config['callback_url']);
-		if ($_SERVER['REQUEST_URI'] === $callbackURI)
+		if (preg_match( '/^'
+					. preg_quote($this->opauth->config['callback_url'], '/')
+					. '(\?[^\/]*)?$/',
+					$_SERVER['REQUEST_URI']))
 		{
+			if (array_key_exists('state', $_GET)
+					&& $_GET['state'] != get_current_blog_id())
+			{
+				$details = get_blog_details($_GET['state']);
+				if ($details !== false)
+				{
+					self::redirectWithPost($details->siteurl
+							. $this->opauth->config['callback_url']);
+					die;
+				}
+			}
 			$this->callback();
-			exit;
+			die;
 		}
 		/* Run opauth only when in the right path. */
 		else if (preg_match(
-					'/^' . preg_quote($this->opauth->config['path'], '/') . '/',
-					$_SERVER['REQUEST_URI']))
+					'/^' . preg_quote($this->opauth->config['path'], '/') . '(.*)/',
+					$_SERVER['REQUEST_URI'], $matches))
 		{
+			/* Intercept callback GET to pass state to the generic callback */
+			$params = explode('/', $matches[1]);
+			if (sizeof($params) >= 2
+					&& !empty($params[0]) && !empty($params[1])
+					&& array_key_exists('state', $_GET)) {
+				$this->opauth->env['callback_url'] .= '?state=' . $_GET['state'];
+			}
+
 			$this->opauth->run();
+			die;
 		}
 	}
 
-	public function callback()
+	private function callback()
 	{
-		$response = null;
-
-		switch($this->opauth->env['callback_transport']) {
-			case 'session':
-				session_start();
-				if (array_key_exists('opauth', $_SESSION))
-				{
-					$response = $_SESSION['opauth'];
-					unset($_SESSION['opauth']);
-				}
-				break;
-			case 'post':
-				$response = unserialize(base64_decode($_POST['opauth']));
-				break;
-			case 'get':
-				$response = unserialize(base64_decode($_GET['opauth']));
-				break;
-		}
+		$response = unserialize(base64_decode($_POST['opauth']));
 
 		var_dump($response);
+	}
+
+	private static function redirectWithPost($url)
+	{
+		echo '<form action="' . htmlentities($url)
+			. '" method="post" name="redirect_form">';
+		foreach ($_POST as $name => $value)
+		{
+			echo '<input type="hidden" name="' . htmlentities($name)
+				. '" value="' . htmlentities($value) . '">';
+		}
+		echo '</form>';
+		echo '<script language="Javascript">';
+		echo "document.redirect_form.submit()";
+		echo "</script>";
 	}
 }
 
